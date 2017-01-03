14fede2c01ffc39cb50ac919b2938c72463f0bad
yt3437 <ytliu6@gmail.com>
2017-01-03 11:00:45 +0800

    123

--- TensorFlow-Exercise/common/shudu.py
+++ TensorFlow-Exercise/common/shudu.py
@@ -1,22 +1,22 @@
-def cross(A,B):
+def cross(A, B):
     return[a+b for a in A for b in B]
 
 digits = '123456789'
 rows = 'ABCDEFGHI'
 cols = digits
-squares = cross(rows,cols)
-unitlist = ([cross(rows,c) for c in cols] +
-            [cross(r,cols) for r in rows] +
-            [cross(rs,cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
-units = dict((s,[u for u in unitlist if s in u]) for s in squares)
-peers = dict((s,set(sum(units[s],[]))-set([s]))for s in squares)
+squares = cross(rows, cols)
+unitlist = ([cross(rows, c) for c in cols] +
+            [cross(r, cols) for r in rows] +
+            [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')])
+units = dict((s, [u for u in unitlist if s in u]) for s in squares)
+peers = dict((s, set(sum(units[s], []))- set([s]))for s in squares)
 
 def test():
     assert len(squares) == 81
-    assert len(unitlist)==27
-    assert all(len(units[s])==3 for s in squares)
-    assert all(len(peers[s])==20 for s in squares)
-    assert units['C2'] ==[['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+    assert len(unitlist) == 27
+    assert all(len(units[s]) == 3 for s in squares)
+    assert all(len(peers[s]) == 20 for s in squares)
+    assert units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
                            ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
                            ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
     assert peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
@@ -25,20 +25,20 @@ def test():
     print('All tests pass.')
 
 def parse_grid(grid):
-    values = dict((s,digits)for s in squares)
-    for s,d in grid_values(grid).items():
-        if d in digits and not assign(values,s,d):
+    values = dict((s, digits)for s in squares)
+    for s, d in grid_values(grid).items():
+        if d in digits and not assign(values, s, d):
             return False
     return values
 
 def grid_values(grid):
     chars=[c for c in grid if c in digits or c in '0.']
-    assert len(chars)==81
+    assert len(chars) == 81
     return dict(zip(squares,chars))
 
-def assign(values,s,d):
-    other_values = values[s].replace(d,'')
-    if all(eliminate(values,s,d2) for d2 in other_values):
+def assign(values, s, d): 
+    other_values = values[s].replace(d, '')
+    if all(eliminate(values, s, d2) for d2 in other_values):
         return values
     else:
         return False
